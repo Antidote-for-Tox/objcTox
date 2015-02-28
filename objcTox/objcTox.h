288b354857aa0e56168cdca31deddfb0d7dfba06
Dmytro Vorobiov <d@dvor.me>
2015-02-28 02:03:36 +0100

    Initial commit

---
+++ objcTox/objcTox.h
@@ -0,0 +1,13 @@
+//
+//  objcTox.h
+//  objcTox
+//
+//  Created by Dmytro Vorobiov on 28.02.15.
+//  Copyright (c) 2015 dvor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface objcTox : NSObject
+
+@end
---
+++ objcTox/objcTox.m
@@ -0,0 +1,13 @@
+//
+//  objcTox.m
+//  objcTox
+//
+//  Created by Dmytro Vorobiov on 28.02.15.
+//  Copyright (c) 2015 dvor. All rights reserved.
+//
+
+#import "objcTox.h"
+
+@implementation objcTox
+
+@end
