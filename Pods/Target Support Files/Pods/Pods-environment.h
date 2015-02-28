53347f841b3d3d94f16b2885e4691acd3c2d9eab
Dmytro Vorobiov <d@dvor.me>
2015-02-28 02:08:47 +0100

    Added toxcore-ios pod

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// libsodium
+#define COCOAPODS_POD_AVAILABLE_libsodium
+#define COCOAPODS_VERSION_MAJOR_libsodium 1
+#define COCOAPODS_VERSION_MINOR_libsodium 0
+#define COCOAPODS_VERSION_PATCH_libsodium 1
+
+// toxcore-ios
+#define COCOAPODS_POD_AVAILABLE_toxcore_ios
+#define COCOAPODS_VERSION_MAJOR_toxcore_ios 0
+#define COCOAPODS_VERSION_MINOR_toxcore_ios 1
+#define COCOAPODS_VERSION_PATCH_toxcore_ios 0
+
