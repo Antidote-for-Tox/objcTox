244940000b198f0978fa72f6935e43d85c476228
Dmytro Vorobiov <d@dvor.me>
2015-03-10 23:39:58 +0100

    Added OCTFriend

---
+++ Classes/Manager/Objects/OCTFriend.m
@@ -0,0 +1,13 @@
+//
+//  OCTFriend.m
+//  objcTox
+//
+//  Created by Dmytro Vorobiov on 10.03.15.
+//  Copyright (c) 2015 dvor. All rights reserved.
+//
+
+#import "OCTFriend.h"
+
+@implementation OCTFriend
+
+@end
