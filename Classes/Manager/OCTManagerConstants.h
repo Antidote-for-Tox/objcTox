27dd643a9ae808f43e3d4703bc5b4bf44448defc
Dmytro Vorobiov <d@dvor.me>
2015-03-15 21:32:11 +0100

    Added OCTManagerFriendsProtocol

---
+++ Classes/Manager/OCTManagerConstants.h
@@ -0,0 +1,12 @@
+//
+//  OCTManagerConstants.h
+//  objcTox
+//
+//  Created by Dmytro Vorobiov on 15.03.15.
+//  Copyright (c) 2015 dvor. All rights reserved.
+//
+
+typedef NS_ENUM(NSUInteger, OCTManagerFriendsSort) {
+    OCTManagerFriendsSortByName,
+    OCTManagerFriendsSortByStatus,
+};
