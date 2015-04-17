f3437b02b9c97457b61dfa05e7bfbe69002c53ca
Dmytro Vorobiov <d@dvor.me>
2015-04-17 21:16:28 +0200

    Added FriendRequestContainer

---
+++ Classes/Private/Manager/Submanagers/OCTFriendRequestContainer.m
@@ -0,0 +1,13 @@
+//
+//  OCTFriendRequestContainer.m
+//  objcTox
+//
+//  Created by Dmytro Vorobiov on 17.04.15.
+//  Copyright (c) 2015 dvor. All rights reserved.
+//
+
+#import "OCTFriendRequestContainer.h"
+
+@implementation OCTFriendRequestContainer
+
+@end
---
+++ Classes/Public/Manager/Submanagers/OCTFriendRequestContainer.h
@@ -0,0 +1,13 @@
+//
+//  OCTFriendRequestContainer.h
+//  objcTox
+//
+//  Created by Dmytro Vorobiov on 17.04.15.
+//  Copyright (c) 2015 dvor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OCTFriendRequestContainer : NSObject
+
+@end
