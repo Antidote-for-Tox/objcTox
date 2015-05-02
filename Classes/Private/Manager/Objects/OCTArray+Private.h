4c1aa711c88a8c13f16903580b52420ecb302f79
Dmytro Vorobiov <d@dvor.me>
2015-05-02 14:19:28 +0200

    Added OCTArray with empty implementation, OCTSortDescriptor

---
+++ Classes/Private/Manager/Objects/OCTArray+Private.h
@@ -0,0 +1,13 @@
+//
+//  OCTArray+Private.h
+//  objcTox
+//
+//  Created by Dmytro Vorobiov on 02.05.15.
+//  Copyright (c) 2015 dvor. All rights reserved.
+//
+
+#import "OCTArray.h"
+
+@interface OCTArray (Private)
+
+@end
---
+++ Classes/Private/Manager/Objects/OCTArray.m
@@ -0,0 +1,40 @@
+//
+//  OCTArray.m
+//  objcTox
+//
+//  Created by Dmytro Vorobiov on 02.05.15.
+//  Copyright (c) 2015 dvor. All rights reserved.
+//
+
+#import "OCTArray.h"
+
+@implementation OCTArray
+
+#pragma mark -  Public
+
+- (NSObject *)firstObject
+{
+    return nil;
+}
+
+- (NSObject *)lastObject
+{
+    return nil;
+}
+
+- (NSObject *)objectAtIndex:(NSUInteger)index
+{
+    return nil;
+}
+
+- (OCTArray *)sortedObjectsUsingDescriptors:(NSArray *)descriptors
+{
+    return nil;
+}
+
+- (void)enumerateObjectsUsingBlock:(void (^)(NSObject *obj, NSUInteger idx, BOOL *stop))block
+{
+
+}
+
+@end
