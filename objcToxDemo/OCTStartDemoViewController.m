18cef1bb57ef9711e16d45141a20e3e6267a1d18
Dmytro Vorobiov <d@dvor.me>
2015-05-20 08:01:37 +0200

    Adding demo target

---
+++ objcToxDemo/OCTStartDemoViewController.m
@@ -0,0 +1,25 @@
+//
+//  OCTStartDemoViewController.m
+//  objcTox
+//
+//  Created by Dmytro Vorobiov on 20/05/15.
+//  Copyright (c) 2015 dvor. All rights reserved.
+//
+
+#import "OCTStartDemoViewController.h"
+#import "OCTManager.h"
+
+@interface OCTStartDemoViewController ()
+
+@end
+
+@implementation OCTStartDemoViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    self.title = @"Start";
+}
+
+@end
