b46fd0c3efd7b9bc552f6b41d61d6228658aa366
Chuong Vu <chuongv@gmail.com>
2015-06-04 15:41:18 -0700

    Add OCTSubmanagerCalls+Private and CallsTest

---
+++ Classes/Private/Manager/Submanagers/OCTSubmanagerCalls+Private.h
@@ -0,0 +1,21 @@
+//
+//  OCTSubmanagerCalls+Private.h
+//  objcTox
+//
+//  Created by Chuong Vu on 6/4/15.
+//  Copyright (c) 2015 dvor. All rights reserved.
+//
+
+#import "OCTSubmanagerCalls.h"
+
+@class OCTTox;
+
+@interface OCTSubmanagerCalls (Private)
+
+/**
+ * Initialize the OCTSubmanagerCall
+ *
+ */
+- (instancetype)initWithTox:(OCTTox *)tox;
+
+@end
