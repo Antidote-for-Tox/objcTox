1c2fafafc1f5d660b8215dff76b3b8eedaa92269
Chuong Vu <chuongv@gmail.com>
2015-12-13 23:06:03 -0800

    OSX Demo: Tabview

--- OSXDemo/OCTMainWindowController.m
+++ OSXDemo/OCTMainWindowController.m
@@ -21,11 +21,13 @@ const NSInteger kChatsTag = 2;
 @interface OCTMainWindowController () <OCTBootStrapViewDelegate>
 
 @property (weak) IBOutlet NSView *mainView;
-@property (weak) IBOutlet NSPopUpButton *dropDownButton;
 @property (strong, nonatomic) NSViewController *currentViewController;
+@property (strong, nonatomic) NSViewController *userViewController;
+@property (strong, nonatomic) NSViewController *friendUserViewController;
 
 @property (strong, nonatomic) OCTManagerConfiguration *configuration;
 @property (strong, nonatomic) OCTManager *manager;
+@property (weak) IBOutlet NSTabView *tabView;
 
 @end
 
@@ -36,7 +38,7 @@ const NSInteger kChatsTag = 2;
     [super windowDidLoad];
 
     self.configuration = [OCTManagerConfiguration defaultConfiguration];
-    self.dropDownButton.hidden = YES;
+    self.tabView.hidden = YES;
 
     [self setupBootstrapViewController];
 }
@@ -46,16 +48,11 @@ const NSInteger kChatsTag = 2;
     OCTBootStrapViewController *bootstrapVC = [[OCTBootStrapViewController alloc]
                                                initWithConfiguration:self.configuration];
     bootstrapVC.delegate = self;
+
     self.currentViewController = bootstrapVC;
     [self.mainView addSubview:self.currentViewController.view];
     [self.currentViewController.view setFrame:self.mainView.bounds];
 }
-
-- (IBAction)changeViewSelected:(NSPopUpButtonCell *)sender
-{
-    [self changeViewControllerForTag:sender.selectedItem.tag];
-}
-
 
 #pragma mark - Bootstrap delegate
 
@@ -66,64 +63,23 @@ const NSInteger kChatsTag = 2;
     self.manager = [[OCTManager alloc] initWithConfiguration:self.configuration error:nil];
     [self.manager.bootstrap addPredefinedNodes];
     [self.manager.bootstrap bootstrap];
-    self.dropDownButton.hidden = NO;
-    [self.dropDownButton selectItemAtIndex:kUserTag];
-    [self changeViewControllerForTag:kUserTag];
+
+    self.tabView.hidden = NO;
+
+    [self setupTabControllers];
 }
 
 #pragma mark - Private
 
-- (void)changeViewControllerForTag:(NSInteger)tag
+- (void)setupTabControllers
 {
-    if (self.currentViewController) {
-        [self.currentViewController.view removeFromSuperview];
-        self.currentViewController = nil;
-    }
+    NSTabViewItem *userViewItem = [self.tabView tabViewItemAtIndex:0];
+    self.userViewController = [[OCTUserViewController alloc] initWithManager:self.manager.user];
+    userViewItem.view = self.userViewController.view;
 
-    switch (tag) {
-        case kUserTag:
-            [self switchToUsersWindow];
-            break;
-        case kChatsTag:
-            break;
-        case kFriendsTag:
-            [self switchToFriendWindow];
-            break;
-    }
-}
-
-- (void)switchToUsersWindow
-{
-    OCTUserViewController *userViewController = [[OCTUserViewController alloc] initWithManager:self.manager.user];
-    self.currentViewController = userViewController;
-    [self.mainView addSubview:self.currentViewController.view];
-    self.currentViewController.view.frame = self.mainView.bounds;
-}
-
-- (void)switchToChatWindow
-{}
-
-- (void)switchToFriendWindow
-{
-    OCTFriendsViewController *friendsViewController = [[OCTFriendsViewController alloc] initWithManager:self.manager];
-    self.currentViewController = friendsViewController;
-    [self.mainView addSubview:self.currentViewController.view];
-
-    NSView *view = self.currentViewController.view;
-    NSDictionary *viewsDictionary = NSDictionaryOfVariableBindings(view);
-    [self.mainView addConstraints:[NSLayoutConstraint
-                                              constraintsWithVisualFormat:@"V:|[view]|"
-                                                                options:0
-                                                                metrics:nil
-                                                                views:viewsDictionary]];
-
-    [self.mainView addConstraints:[NSLayoutConstraint
-                                              constraintsWithVisualFormat:@"H:|[view]|"
-                                              options:0
-                                              metrics:nil
-                                              views:viewsDictionary]];
-
-    self.currentViewController.view.frame = self.mainView.bounds;
+    NSTabViewItem *friendViewItem = [self.tabView tabViewItemAtIndex:1];
+    self.friendUserViewController = [[OCTFriendsViewController alloc] initWithManager:self.manager];
+    friendViewItem.view = self.friendUserViewController.view;
 }
 
 @end
